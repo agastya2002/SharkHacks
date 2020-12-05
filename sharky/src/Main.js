2b95526666ed53072c309b9c75335660a1fe24b7
agastya2002 <agastya2002@gmail.com>
2020-12-05 21:56:58 +0530

    Autocomplete and Display

--- sharky/src/Main.js
+++ sharky/src/Main.js
@@ -3,19 +3,97 @@ import Chattershark from './Chattershark';
 import Response from './Response';
 import Graph from './Graph';
 import { Component } from 'react';
-
+import companies from './nasdaq.json';
 
 
 
 class Main extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+          message: "Hello I'm Sharky! Search your company from the list",
+          name: ""
         }
+    }
+    componentDidMount() {
+      console.log('compdidmount')
+      // const name = ""
+      const search = document.getElementById('search');
+      const matchList = document.getElementById('match-list');
+      const searchComps = searchText => {
+        let matches = companies.filter(company => {
+            const regex = new RegExp(`^${searchText}`, 'gi')
+            return (company.name.match(regex) || company.symbol.match(regex));
+        });
+        // console.log(matches);
+        if (searchText.length === 0) {
+            matches = [];
+            matchList.innerHTML = '';
+      
+        }
+        // console.log(matches);
+        outputHTML(matches);
+      };
+      
+      const outputHTML = matches => {
+        if(matches.length > 0) {
+          const HTML = matches.map(match => `
+            <div class = "card card-body mb-1">
+              <h5 class= "choice" id = "${match.symbol}|${match.name}">${match.name} (${match.symbol})</h5> 
+            </div>
+          `).join('');
+          matchList.innerHTML = HTML;
+        }
+      }
+      
+      search.addEventListener('input',() => {
+        searchComps(search.value)
+      });
+      
+      matchList.addEventListener('click', e =>{
+        matchList.innerHTML = '';
+        search.value = '';
+        search.style.display = 'none';
+        if(e.target.classList.contains('choice')) {
+          const details = e.target.id.split('|');
+          this.setState({
+            name: details[1],
+            message: `Please wait for a while. My fishies are hard at work crunching numbers! <div class="dot-flashing"></div>`
+          });
+          console.log('Fetching details for ', details[1]);
+          fetch(`https://f736c9037e08.ngrok.io/sent/${details[0]}`)
+          .then(response=>response.json())
+          .then(data => {
+            search.style.display = 'block';
+            if(data.check === 0){
+              this.setState({
+                message: `
+                The data gathered on ${this.state.name} by my loyal fishies is as follows<br>
+                Positive sentiment: ${data.positive}<br>
+                Negative sentiment: ${data.negative}<br>
+                Analysing the data I predict todays price to be $${data.price}
+              `
+              });
+            } else {
+              this.setState({
+                message: `
+                The data gathered on ${this.state.name} by my loyal fishies is as follows<br>
+                Positive sentiment: ${data.positive}<br>
+                Negative sentiment: ${data.negative}<br>
+                ${data.price}
+              `
+            });
+          }
+            console.log(this.state);});
+        }
+      });
+    }
     render() {
+      console.log('reder');
         return(
             <div className="body-container">
                 <Chattershark/>
-                <Response smthn={this.props.smthn}/>
+                <Response message = {this.state.message}/>
                 <Graph />
             </div>
         )
--- sharky/src/Response.js
+++ sharky/src/Response.js
@@ -1,38 +1,27 @@
 import './Response.css';
 import $ from 'jquery';
+import { Component } from 'react';
 import Typewriter from 'typewriter-effect';
 
-function Response(props) {
-    return (
-        <div className="response">
-            <div className="bubble align-text-center" id="response bubble">
-                <Typewriter
-                options = {{delay: 30}}
-                    onInit={(typewriter) => {
-                        typewriter.typeString(props.smthn)
-                        .callFunction(() => {
-                            console.log('String typed out!');
-                        })
-                        .pauseFor(500)
-                        .deleteAll()
-                        .pauseFor(300)
-                        .typeString("Search your company from the list")
-                        .pauseFor(2500)
-                        .deleteAll()
-                        .typeString(props.smthn)
-                        .start()
-                    }}
-                />
-                <i className="fa fa-spinner fa-pulse fa-3 fa-fw text-primary d-none w-100"></i>
-            </div>
-        </div>
-    );
+class Response extends Component {
+    constructor(props){
+      super(props);
+    }
+    componentDidMount() {
+      // this.forceUpdate();
+    }
+    render() {
+      console.log(this.props.message);
+      return (
+          <div className="response">
+              <div className="bubble align-text-center" id="response bubble">
+                  <Typewriter
+                    options = {{delay: 30, autoStart:true, strings:[this.props.message,], pauseFor:900000, cursorClassName:'cursor'}}  
+                  />
+              </div>
+          </div>
+      );
+    }
 }
-
-
-// fetch('https://248a15902960.ngrok.io/sent/AAPL')
-// .then(response=>response.json())
-// .then(data => console.log(data));
-
 
 export default Response;
