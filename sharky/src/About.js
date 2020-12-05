774f5e7a8e0a3ffe399456bf285167b37ae72c7e
Malab Sankar Barik <malab@outlook.com>
2020-12-05 15:04:28 +0530

    typewriter affect and about us images

--- sharky/src/About.js
+++ sharky/src/About.js
@@ -1,5 +1,9 @@
 import React from "react";
 import './About.css'
+import Agastya from './img/Agastya.png';
+import Arnav from './img/Arnav.png';
+import Malab from './img/Malab.png';
+import Simran from './img/Simran.png';
 
 const About = () => (
   <div>
@@ -13,35 +17,35 @@ const About = () => (
     <h3 className= "text-center mt-5">Meet the Team</h3>
     <div className="container mt-5">
         <div className="card d-inline-block mx-1" style={{width: "24%"}}>
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Agastya} className="img shark mx-auto d-block" alt="..." />
         <div className="card-body">
             <h5 className="card-title text-center text-dark">Agastya</h5>
-            <p className="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p className="card-text text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             <a href="#" className="btn btn-outline-dark">Go somewhere</a>
         </div>
         </div>
         <div className="card d-inline-block mx-1" style={{width: "24%"}}>
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Arnav} className="img shark mx-auto d-block" alt="..." />
         <div className="card-body">
             <h5 className="card-title text-center text-dark">Arnav</h5>
-            <p className="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p className="card-text text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             <a href="#" className="btn btn-outline-dark">Go somewhere</a>
         </div>
         </div>
         <div className="card d-inline-block mx-1" style={{width: "24%"}}>
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Malab} className="img shark mx-auto d-block" alt="..." />
         <div className="card-body">
             <h5 className="card-title text-center text-dark">Malab</h5>
-            <p className="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p className="card-text text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             <a href="#" className="btn btn-outline-dark">Go somewhere</a>
         </div>
         </div>
         <div className="card d-inline-block mx-1" style={{width: "24%"}}>
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={Simran} className="img shark mx-auto d-block" alt="..." />
         <div className="card-body">
             <h5 className="card-title text-center text-dark">Simran</h5>
-            <p className="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-outline-dark">Go somewhere</a>
+            <p className="card-text text-secondary">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-outline-dark">Go to my Github!</a>
         </div>
         </div>
         
--- sharky/src/Response.js
+++ sharky/src/Response.js
@@ -1,14 +1,30 @@
 import './Response.css';
+import $ from 'jquery';
+import Typewriter from 'typewriter-effect';
 
 function Response() {
     return (
         <div className="response">
             <div className="bubble align-text-center" id="response bubble">
-                Hello My name is Sharky!
+                <Typewriter
+                options = {{delay: 30}}
+                    onInit={(typewriter) => {
+                        typewriter.typeString('Hello my name is Sharky the shark! I am here to help you with recommendations about the stock market!')
+                        .callFunction(() => {
+                            console.log('String typed out!');
+                        })
+                        .pauseFor(500)
+                        .deleteAll()
+                        .pauseFor(300)
+                        .typeString("Search your company from the list")
+                        .start()
+                    }}
+                />
                 <i className="fa fa-spinner fa-pulse fa-3 fa-fw text-primary d-none w-100"></i>
             </div>
         </div>
     );
 }
+
 
 export default Response;
